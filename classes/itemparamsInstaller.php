2a78f755fa58d13535f6b886dd96bbd88bade940
ralferlebach <ralf.erlebach@gmx.de>
2026-02-16 23:15:03 +0100

    Update itemparamsInstaller.php

--- classes/itemparamsInstaller.php
+++ classes/itemparamsInstaller.php
@@ -36,7 +36,6 @@ use Exception;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class itemparamsInstaller extends wbInstaller {
-
     /**
      * Entities constructor.
      * @param array $recipe
